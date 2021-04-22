081a9421a2074dd26cc6a732209feb4564cced88
Alberto <albgp22@gmail.com>
2021-04-22 17:01:01 +0200

    Add watch option in FTPHandler

--- FTPHandler/FTPMethods.py
+++ FTPHandler/FTPMethods.py
@@ -3,6 +3,77 @@ import configparser
 import os
 from pathlib import Path
 
+from collections import defaultdict
+
+class TrieNode():
+
+    def __init__(self):
+        self.children = defaultdict()
+        self.terminating = False
+
+
+class Trie():
+
+    def __init__(self):
+        self.root = self.get_node()
+
+    def get_node(self):
+        return TrieNode()
+
+    def get_index(self, ch):
+        return ord(ch) - ord('a')
+
+    def insert(self, word):
+
+        root = self.root
+        len1 = len(word)
+
+        for i in range(len1):
+            index = self.get_index(word[i])
+
+            if index not in root.children:
+                root.children[index] = self.get_node()
+            root = root.children.get(index)
+
+        root.terminating = True
+
+    def search(self, word):
+        root = self.root
+        len1 = len(word)
+
+        for i in range(len1):
+            index = self.get_index(word[i])
+            if not root:
+                return False
+            root = root.children.get(index)
+
+        return True if root and root.terminating else False
+
+    def delete(self, word):
+
+        root = self.root
+        len1 = len(word)
+
+        for i in range(len1):
+            index = self.get_index(word[i])
+
+            if not root:
+                print ("Word not found")
+                return -1
+            root = root.children.get(index)
+
+        if not root:
+            print ("Word not found")
+            return -1
+        else:
+            root.terminating = False
+            return 0
+
+    def update(self, old_word, new_word):
+        val = self.delete(old_word)
+        if val == 0:
+            self.insert(new_word)
+
 def readConfig():
     config = configparser.ConfigParser()
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -25,18 +96,27 @@ def connectFTP(config):
     return ftp
     
 def doOverFTP(f):
+    trie = Trie()
     config=readConfig()
     print(config)
     ftp=connectFTP(config)
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     images_path = os.path.join(Path(__location__).parent,'Images')
-    filenames = ftp.nlst() # get filenames within the directory
-    for filename in filenames:
-        local_filename=os.path.join(images_path, filename)
-        file = open(local_filename, 'wb')
-        ftp.retrbinary('RETR '+ filename, file.write)
-        file.close
-        f(local_filename)
+    already_processed=[]
+    while True:
+        filenames = ftp.nlst() # get filenames within the directory
+        for filename in filenames:
+            if not trie.search(filename):
+                trie.insert(filename)
+                already_processed.append(filename)
+                local_filename=os.path.join(images_path, filename)
+                file = open(local_filename, 'wb')
+                ftp.retrbinary('RETR '+ filename, file.write)
+                file.close
+                f(local_filename)
+        if config['watch']=="false":
+            print("Not watching: exiting")
+            break
 
     
 
